d57a067fbab16961becf14677d15eb9c034703af
apalah <155531891+axo-id@users.noreply.github.com>
2024-07-06 15:22:48 +0700

    Update config.js

--- scripts/main/config.js
+++ scripts/main/config.js
@@ -1,13 +1,13 @@
 // ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
 const firebaseConfig = {
-    apiKey: "AIzaSyBSUG08nPQyjvx8cCskV2CHqF_ILvxjU98",
-    authDomain: "chats-app-12.firebaseapp.com",
-    databaseURL: "https://chats-app-12-default-rtdb.firebaseio.com",
-    projectId: "chats-app-12",
-    storageBucket: "chats-app-12.appspot.com",
-    messagingSenderId: "670073879651",
-    appId: "1:670073879651:web:dc134fb1dde15710478c56",
-    measurementId: "G-C88E1KS3XY"
+  apiKey: "AIzaSyAr6x-Dt7VurVzA9VZWUBr7v39AvtE2jVo",
+  authDomain: "axoo-id.firebaseapp.com",
+  databaseURL: "https://axoo-id-default-rtdb.firebaseio.com",
+  projectId: "axoo-id",
+  storageBucket: "axoo-id.appspot.com",
+  messagingSenderId: "543674574805",
+  appId: "1:543674574805:web:e08d7e6f3050b0e479ff5c",
+  measurementId: "G-YM5M7M0N1H"
 };
 
 // BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
