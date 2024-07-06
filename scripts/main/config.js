b0acbe07574818358b2affbe2ec59cf805ac6239
apalah <155531891+axo-id@users.noreply.github.com>
2024-07-06 10:48:17 +0700

    Update config.js

--- scripts/main/config.js
+++ scripts/main/config.js
@@ -1,12 +1,13 @@
 // ISI DENGAN PROJECT FIREBASE MILIK TEMEN-TEMEN SENDIRI
 const firebaseConfig = {
-    apiKey: "PUNYA-KAMU",
-    authDomain: "PUNYA-KAMU",
-    databaseURL: "PUNYA-KAMU",
-    projectId: "PUNYA-KAMU",
-    storageBucket: "PUNYA-KAMU",
-    messagingSenderId: "PUNYA-KAMU",
-    appId: "PUNYA-KAMU"
+    apiKey: "AIzaSyBSUG08nPQyjvx8cCskV2CHqF_ILvxjU98",
+    authDomain: "chats-app-12.firebaseapp.com",
+    databaseURL: "https://chats-app-12-default-rtdb.firebaseio.com",
+    projectId: "chats-app-12",
+    storageBucket: "chats-app-12.appspot.com",
+    messagingSenderId: "670073879651",
+    appId: "1:670073879651:web:dc134fb1dde15710478c56",
+    measurementId: "G-C88E1KS3XY"
 };
 
 // BIAR GA KELAMAAN PAS MAU PANGGIL NANTI
